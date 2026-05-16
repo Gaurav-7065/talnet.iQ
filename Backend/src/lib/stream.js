f4126ff7d0e5afaffc39927dec7cd64c3291c4c3
Gaurav Jha <jhagaurav214@gmail.com>
2026-05-16 10:57:53 +0530

    inggest to strem data transfer

---
+++ Backend/src/lib/stream.js
@@ -0,0 +1,31 @@
+import { StreamChat } from "stream-chat";
+import { ENV } from "./env";
+
+const apiKey=ENV.STREAM_API_KEY;
+const apiStream=ENV.STREAM_API_SECRET;
+if(!apiKey||!apiStream){
+    console.error("STREAM_API_KEY && STREAM_API_SECRET are invalid")
+}
+
+export const chatClient=StreamChat.getInstance(apiKey,apiStream);
+export const upsertStramUser=async (userData)=>{
+
+    try{
+        await chatClient.upsertUser(userData);
+        console.log("user get upserted succesfully",userData);
+    }
+    catch(error){
+        console.error("Error upserting Stream User"+error);
+    }
+
+}
+export const deleteUser=async(userID)=>{
+    try{
+        await chatClient.deleteUser(userID);
+        console.log("user get deleted succesfully",userID)
+    }
+    catch(error){
+        console.error("Error deleting the stream User",error)
+    }
+}
+//todo add another to genr token
