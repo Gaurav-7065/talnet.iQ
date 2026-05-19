4282538f33504a7bde8eda6d6b436cd45263be4b
Gaurav Jha <jhagaurav214@gmail.com>
2026-05-19 13:10:44 +0530

    fixed import problem

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -29,7 +29,7 @@ if(!isLoaded) return null;
         <Route path='/dashboard' element={isSignedIn?<DashboardPage/>:<Navigate to={"/"}/>}/>
 
         
-        <Route path='/problems' element={isSignedIn?<ProblemsPages/>:<Navigate to={"/"}/>}/>
+        <Route path='/problems' element={isSignedIn?<ProblemsPage/>:<Navigate to={"/"}/>}/>
         <Route path='/problem/:id' element={isSignedIn?<ProblemPage/>:<Navigate to={"/"}/>}/>
         <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
 
