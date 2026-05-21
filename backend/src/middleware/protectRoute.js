0c96fc329075752b7a8c2709368c01cc08d59eca
Gaurav Jha <jhagaurav214@gmail.com>
2026-05-21 19:00:05 +0530

    fixed issue

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,24 +1,26 @@
 import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
 
+export const protectRoute = [
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth().userId;
 
-export const protectRoute=[
-    requireAuth(),
-    async (req,res,next)=>{
-        try{
-            const clerkId=req.auth().userId 
-            if(!clerkId) return res.status(401).json({message:"unauthorized invalid token"})
-            
-            const user=await User.findOne({clerkId})
-            if(!user) return res.status(404).json({message:"User not found "})
+      if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
 
-            req.user=user;
-            next();
+      // find user in db by clerk ID
+      const user = await User.findOne({ clerkId });
 
-        }
-        catch(error){
-            console.error("Error in protectec route middleware",error);
-            res.status(500).json({msg:"Internal server error"});
-        }
+      if (!user) return res.status(404).json({ message: "User not found" });
+
+      // attach user to req
+      req.user = user;
+
+      next();
+    } catch (error) {
+      console.error("Error in protectRoute middleware", error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
-]
+  },
+];
