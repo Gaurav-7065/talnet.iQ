bb9e0cf6a2729f7456c8e86f32d4167c269eda2d
Gaurav Jha <jhagaurav214@gmail.com>
2026-05-21 15:46:46 +0530

    changes done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance=axios.create({
-    baseURL:import.meta.env.VITE_API_URL,
+    baseURL:`${import.meta.env.VITE_API_URL/api}`,
     withCredentials:true//Brower will send cokkies automaticaly to the server on every single req
 });
 export default axiosInstance;
